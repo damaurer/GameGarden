91a599d03c237e6737c19834ac3e3e015c7dc886
Maurer Michael <michael.maurer1991@outlook.de>
2024-07-11 15:54:27 +0200

    Remove keycloak/postgres environment and configuration files
    Add Apache Derby support

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,15 +20,19 @@ repositories {
 }
 
 dependencies {
+    runtimeOnly("org.apache.derby:derby:10.17.1.0")
+    implementation("org.apache.derby:derbytools:10.17.1.0")
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.liquibase:liquibase-core")
+    implementation("com.google.guava:guava:30.0")
+    implementation("org.apache.commons:commons-compress:1.26")
     implementation("com.github.docker-java:docker-java-core:3.3.6")
     implementation("com.github.docker-java:docker-java-transport-httpclient5:3.3.6")
-    runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
